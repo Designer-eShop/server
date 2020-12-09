7dbc29dfa7f7a9f1b45182448cd81aeb70bf7e2d
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-12-09 22:42:06 +0200

    Added generic login, register and clothes tables

--- server.js
+++ server.js
@@ -2,48 +2,191 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const middleware = require("./middleware");
 require("dotenv").config();
 
-const port = process.env.SERVER_PORT || 8080;
+const database = require("./db");
 
-const con = mysql.createConnection({
-  host: process.env.MYSQL_DB_HOST,
-  user: process.env.MYSQL_DB_USER,
-  password: process.env.MYSQL_DB_PASS,
-  database: process.env.MYSQL_DB_NAME,
-  port: process.env.MYSQL_DB_PORT,
-});
-
-con.connect((err) => {
-  if (err) throw err;
-  console.log("Successfully connected to DB");
-});
+const port = process.env.SERVER_PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/add-user", (req, res) => {
-  const data = req.body;
-  if (data.fullname && data.company && data.address) {
-    con.query(
-      `INSERT INTO users (name, company, address)
-       VALUES ('${data.fullname}', '${data.company}', '${data.address}')`,
+app.get("/", (req, res) => {
+  res.send("Back-End is working");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.email.toLowerCase();
+  database((db) =>
+    db.query(
+      `SELECT * FROM users WHERE email = '${username}'`,
       (err, result) => {
-        if (err) {
-          res
-            .status(400)
-            .send(
-              "The DB has not added any records due to an internal problem"
-            );
+        if (err || result.length === 0) {
+          res.status(400).json({ msg: "Username or password is incorrect" });
         } else {
-          res.status(201).json({ id: result.insertId });
+          bcrypt.compare(
+            req.body.password,
+            result[0].password,
+            (bErr, bResult) => {
+              if (bErr || !bResult) {
+                res
+                  .status(400)
+                  .json({ msg: "Username or password is incorrect" });
+              } else if (bResult) {
+                const token = jwt.sign(
+                  {
+                    userId: result[0].id,
+                    username,
+                  },
+                  process.env.SECRETKEY,
+                  {
+                    expiresIn: "7d",
+                  }
+                );
+                db.query(
+                  `UPDATE users SET last_login_date = now() WHERE id = '${result[0].id}'`
+                );
+                res.status(200).json({
+                  msg: "Logged in",
+                  id: result[0].id,
+                  token,
+                  userData: {
+                    userId: result[0].id,
+                    username,
+                  },
+                });
+              }
+            }
+          );
         }
       }
+    )
+  );
+});
+
+app.post("/register", middleware.validateRegistration, (req, res) => {
+  const username = req.body.email.toLowerCase();
+  const date = new Date().toISOString().slice(0, 10);
+  database((db) =>
+    db.query(
+      `SELECT * FROM users WHERE email = '${username}'`,
+      (err, result) => {
+        if (err) {
+          res.status(400).json(err);
+        } else if (result.length !== 0) {
+          res.status(400).json("The username already exists");
+        } else {
+          bcrypt.hash(req.body.password, 10, (error, hash) => {
+            if (error) {
+              res.status(400).json(error);
+            } else {
+              db.query(
+                `INSERT INTO users (email, password, last_login_date) VALUES ('${username}', '${hash}', '${date}')`,
+                (err, result) => {
+                  if (err) {
+                    res.status(400).json(err);
+                  } else {
+                    res.status(201).json({
+                      msg: "User has been registered",
+                    });
+                  }
+                }
+              );
+            }
+          });
+        }
+      }
+    )
+  );
+});
+
+app.get("/clothes", middleware.isLoggedIn, (req, res) => {
+  database((db) =>
+    db.query(`SELECT * FROM clothes`, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ msg: "Internal server error" });
+      } else {
+        return res.status(200).json(result);
+      }
+    })
+  );
+});
+
+app.post("/clothes", middleware.isLoggedIn, (req, res) => {
+  if (
+    req.body.title &&
+    req.body.description &&
+    req.body.price &&
+    req.body.image &&
+    req.body.size &&
+    req.body.gender
+  ) {
+    database((db) =>
+      db.query(
+        `INSERT INTO clothes (title, description, price, image, size, gender) VALUES (${mysql.escape(
+          req.body.title
+        )}, ${mysql.escape(req.body.description)}, ${mysql.escape(
+          req.body.price
+        )}, ${mysql.escape(req.body.price)}, ${mysql.escape(
+          req.body.image
+        )}, ${mysql.escape(req.body.size)}, ${mysql.escape(req.body.gender)})`,
+        (err) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).json({ msg: "Internal server error" });
+          } else {
+            return res
+              .status(200)
+              .json({ msg: "Product has been added successfully" });
+          }
+        }
+      )
     );
   } else {
-    res.status(400).send("The information provided is not correct.");
+    return res.status(400).json({ msg: "Information entered incorrectly" });
+  }
+});
+
+app.post("/updateuser", middleware.isLoggedIn, (req, res) => {
+  if (
+    req.body.name &&
+    req.body.surname &&
+    req.body.phone &&
+    req.body.street &&
+    req.body.city &&
+    req.body.zip
+  ) {
+    database((db) =>
+      db.query(
+        `UPDATE users SET name = ${mysql.escape(
+          req.body.name
+        )}, surname = ${mysql.escape(req.body.surname)}, phone = ${mysql.escape(
+          req.body.phone
+        )}, street = ${mysql.escape(req.body.street)}, city = ${mysql.escape(
+          req.body.city
+        )}, zip = ${mysql.escape(req.body.zip)} WHERE id = '${
+          req.userData.userId
+        }'`,
+        (err) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).json({ msg: "Internal server error" });
+          } else {
+            return res
+              .status(200)
+              .json({ msg: "User has been updated succesfully" });
+          }
+        }
+      )
+    );
+  } else {
+    return res.status(400).json({ msg: "Information entered incorrectly" });
   }
 });
 
